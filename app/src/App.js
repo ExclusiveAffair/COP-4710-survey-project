57c5d054661eb5fa7b2a9b38e7a2924d8e3becf8
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2022-11-22 17:34:57 -0500

    Add files via upload

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'; // import react
 import Login from './pages/Login';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Home />
-      </div>
-    );
-  }
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path ="login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
