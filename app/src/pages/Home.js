f9f5d233658972ff1d8512901ba7b9c83e98409c
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2022-11-22 17:35:16 -0500

    Add files via upload

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Navbar from "../components/navbar";
 
-export default function Home () {
+export default function Home (){
     return (
         <div>
+            <Navbar/>
+            <br/>
             <h1>Homepage</h1>
         </div>
     )
