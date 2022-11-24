35125f969b6c7a68f66d6dcc45831d6697dbd5e7
Natalie <nlongtin2024@knights.ucf.edu>
2022-11-24 16:23:57 -0500

    Begin form for creating new survey

--- app/src/pages/Create.js
+++ app/src/pages/Create.js
@@ -3,6 +3,23 @@ const Create = () => {
         // TODO: fill in create form
         <div className="create">
             <h2> Create a new survey </h2>
+            <form>
+                <label>Survey title:</label>
+                <input 
+                    type="text"
+                    required
+                />
+                <label>Survey description:</label>
+                <textarea 
+                    required
+                />
+                <label>Survey type:</label>
+                <select>
+                    <option value="likert">Likert scale</option>
+                    <option value="shortanswer"> Short answer</option>
+                </select>
+                <button>Create Survey</button>
+            </form>
         </div>
     );
 }
