76c9ffe917489f47e977aa3035a86332c8eb2b42
Natalie <nlongtin2024@knights.ucf.edu>
2022-11-26 12:07:23 -0500

    Use hooks to get email address and password from text boxes

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,19 +1,49 @@
 import React from "react";
+import { useState } from "react";
+import axios from 'axios';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // TODO: fill in these methods
+    const handleLogin = () => {
+
+    }
+    const handleRegister = () => {
+
+    }
     return (
         <div>
             <div style={{margin: "auto", width: "25%"}}>
-                <h1>Login</h1>
+                <h2>Login</h2>
                 <form action="">
-                    <label htmlFor="username">Username: </label>
-                    <input type="text" defaultValue=""/><br /><br />
+                    <label htmlFor="emailaddress">Email address: </label>
+                    <input 
+                        type="text"
+                        required
+                        defaultValue=""
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br /><br />
                     <label htmlFor="password">Password: </label>
-                    <input type="password" defaultValue=""/><br /><br />
+                    <input 
+                        type="password"
+                        required
+                        defaultValue=""
+                        onChange={(e) => setPassword(e.target.value)}/><br /><br />
                 </form>
                 <div style={{display: "flex", justifyContent: "space-between"}}>
-                    <button type="button" className="btn btn-primary" style={{width: "50%", margin: "0px 20px", backgroundColor: "#f1356d"}}>Login</button>
-                    <button type="button" className="btn btn-primary" style={{width: "50%", backgroundColor: "#f1356d"}}>Register</button>
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        style={{width: "50%", margin: "0px 20px", backgroundColor: "#f1356d"}}
+                        onClick={handleLogin}>Login</button>
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        style={{width: "50%", backgroundColor: "#f1356d"}}
+                        onClick={handleRegister}>Register</button>
                 </div>
             </div>
         </div> 
