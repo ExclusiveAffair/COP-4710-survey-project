accf9f4e0d1a244e1a931305341f4c575977d3a0
David Ryder <67267592+david-ryder@users.noreply.github.com>
2022-11-30 11:08:24 -0500

    created survey report button

--- app/src/components/SurveyList.js
+++ app/src/components/SurveyList.js
@@ -5,11 +5,12 @@ const SurveyList = ({ surveys, handleDelete }) => {
       <div className="survey-list">
         {surveys.map(survey => (
           <div className="survey-preview" key={survey.id} >
-            <Link to={`/surveys/${survey.id}`}>
-                <h2>{ survey.title }</h2>
-                <p> { survey.description }</p>
-            </Link>
-            <button className="btn" onClick={() => handleDelete(survey.id)}>delete survey</button>
+            <h2>{ survey.title }</h2>
+            <p> { survey.description }</p>
+            <div class="flex-container">
+              <button className="btn" onClick={() => handleDelete(survey.id)}>delete survey</button>
+              <button className="btn"><Link style={{textDecoration: "none", color: "inherit"}} to={`/surveys/${survey.id}`}>survey report</Link></button>
+            </div>
           </div>
         ))}
       </div>
