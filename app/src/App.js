8ab150cf7e560c5db5aea3c89349964a68d7993f
Natalie <nlongtin2024@knights.ucf.edu>
2022-11-30 17:37:20 -0500

    Add database connectivity files

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Create from "./pages/Create";
 import SurveyReport from './components/SurveyReport';
 
-function App(){
+function App() {
   return(
     <BrowserRouter>
       <Routes>
